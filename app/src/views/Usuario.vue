e531e4df4a568719b044b9c874e0546e1bb1ce27
gabriela sanchez maza <gaby020313@gmail.com>
2020-11-22 17:49:54 -0500

    todo bien

--- app/src/views/Usuario.vue
+++ app/src/views/Usuario.vue
@@ -1,6 +1,6 @@
 <template>
   <v-content>
-    <!-- <v-row class="pa-2" align="center">
+    <v-row class="pa-2" align="center">
       <v-col cols="12">
         <h2 class="font-weight-regular text-center">
           Mantenimiento de Usuario
@@ -16,38 +16,60 @@
       </v-col>
       <v-col class="pa-1" align="center">
         <v-col cols="10" align="center">
-          <v-card>
-            <v-card-title align="center">
-              <v-row align="center" justify-center class="text-center pb-0">
+          <v-card style="border-top: 5px solid #1a237e !important">
+            <v-card-title align="center" justify-center class="text-center">
+              <v-row
+                align="center"
+                justify-center
+                class="text-center pb-0 pl-10"
+              >
                 <v-form ref="form" v-model="valid" lazy-validation>
-                  <v-col class="d-flex" align="center" cols="12" sm="12">
-                    <v-select
-                      :items="items"
-                      v-model="atributo"
-                      label="Busqueda Personalizada"
-                      dense
-                      class="pa-4"
-                      :rules="[fieldRules.required]"
-                      @click="limpiarValidate"
-                    ></v-select>
-                    <v-text-field
-                      v-model="busqueda"
-                      label="Search"
-                      single-line
-                      hide-details
-                      :rules="[fieldRules.required]"
-                      @click="limpiarValidate"
-                    ></v-text-field>
-                    <v-btn
-                      icon
-                      color="indigo darken-4"
-                      class="pt-8"
-                      @mousedown="validate"
-                      @click="busquedaUsuario"
+                  <v-row align="center" justify-center class="text-center">
+                    <v-col
+                      class="justify-content-center text-center"
+                      align="center"
+                      cols="6"
                     >
-                      <v-icon>mdi-magnify</v-icon>
-                    </v-btn>
-                  </v-col>
+                      <v-select
+                        :items="items"
+                        v-model="atributo"
+                        label="Busqueda Personalizada"
+                        dense
+                        class="pt-8"
+                        :rules="[fieldRules.required]"
+                        @click="limpiarValidate"
+                      ></v-select>
+                    </v-col>
+                    <v-col
+                      class="justify-content-center text-center"
+                      align="center"
+                      cols="5"
+                    >
+                      <v-text-field
+                        v-model="busqueda"
+                        label="Search"
+                        single-line
+                        hide-details
+                        :rules="[fieldRules.required]"
+                        @click="limpiarValidate"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      class="justify-content-center text-center"
+                      align="center"
+                      cols="1"
+                    >
+                      <v-btn
+                        icon
+                        color="indigo darken-4"
+                        class="pt-8"
+                        @mousedown="validate"
+                        @click="busquedaUsuario"
+                      >
+                        <v-icon>mdi-magnify</v-icon>
+                      </v-btn>
+                    </v-col>
+                  </v-row>
                 </v-form>
               </v-row>
             </v-card-title>
@@ -57,26 +79,49 @@
               :loading="loading"
               :item-class="itemFilaColor"
             >
-              <template v-slot:top>
-                <v-switch
-                  v-model="todosU"
-                  label="Todos los usuarios"
-                  class="pt-0 pl-3"
-                  :disabled="disabled"
-                  @change="changeUsuario"
-                ></v-switch>
-              </template>
               <template v-slot:[`item.index`]="{ item }">
                 {{ usuarios.indexOf(item) + 1 }}
               </template>
-              <template v-slot:[`item.action`]="{ item }">
-                <v-select v-model="cSel" :items="item.path"></v-select>
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      v-bind="attrs"
+                      v-on="on"
+                      color="blue-grey"
+                      class="mr-2"
+                      @click="showEditUsuario(item)"
+                    >
+                      mdi-border-color</v-icon
+                    >
+                  </template>
+                  <span>Editar</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      v-bind="attrs"
+                      v-on="on"
+                      class="mr-2"
+                      :color="item.Vigencia ? 'red lighten-1' : 'green'"
+                      @click="deleteUsuario(item)"
+                      >{{
+                        item.Vigencia
+                          ? "mdi-close-circle-outline"
+                          : "mdi-checkbox-marked-circle-outline"
+                      }}</v-icon
+                    >
+                  </template>
+                  <span>{{
+                    item.Vigencia ? "Dar de baja" : "Dar de alta"
+                  }}</span>
+                </v-tooltip>
               </template>
             </v-data-table>
           </v-card>
         </v-col>
       </v-col>
-    </v-row> -->
+    </v-row>
   </v-content>
 </template>
 
@@ -112,6 +157,7 @@ export default {
       { text: "Nombre", value: "p.Nombres" },
       { text: "Perfil", value: "pf.Nombre" },
       { text: "Local", value: "l.Nombre" },
+      { text: "Acciones", value: "actions", width: "10%" },
     ],
     busqueda: "",
     atributo: "",
