50f78afb9bf24f08011dff3d489b957c4207908e
gabriela sanchez maza <gaby020313@gmail.com>
2020-11-26 22:51:41 -0500

    vamos por buen camino

--- app/src/views/MiPerfil.vue
+++ app/src/views/MiPerfil.vue
@@ -1,7 +1,7 @@
 <template>
   <v-content>
     <v-row align="center" justify-center class="text-center pl-10 pr-10">
-      <v-col cols="12" align="center" justify-center class="text-center">
+      <v-col cols="12" align="center">
         <v-card>
           <v-card-title
             class="headline pa-2"
@@ -12,13 +12,26 @@
           >
             <v-icon style="color: #1a237e !important">mdi-layers</v-icon>
             <h6 class="pl-3">Datos Generales</h6>
+            <v-row align="center">
+              <v-col cols="12" align="end">
+                <v-btn
+                  color="indigo darken-4 white--text"
+                  medium
+                  small
+                  @click="eventoEdit()"
+                >
+                  <span v-if="!guardar">Editar</span>
+                  <span v-else>Guardar</span>
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-card-title>
           <v-divider></v-divider>
           <v-card-text>
             <v-container class="pt-3">
-              <v-row>
-                <v-col cols="4">
-                  <v-avatar color="indigo darken-1" size="50">
+              <v-row align="center">
+                <v-col cols="4" align="center">
+                  <v-avatar color="indigo darken-1" size="70">
                     <span class="white--text headline">{{ split }}</span>
                   </v-avatar>
                   <h4 class="pl-3 pt-1">{{ cargo }}</h4>
@@ -27,26 +40,26 @@
                 </v-col>
                 <v-divider vertical></v-divider>
                 <v-col cols="7">
-                  <v-row class="d-flex" justify-center>
-                    <v-col cols="6" class="d-flex">
+                  <v-row class="d-flex" justify="center">
+                    <v-col cols="6">
                       <v-text-field
                         v-model="nombre"
                         label="Nombre"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="6" class="d-flex">
                       <v-text-field
                         v-model="apPaterno"
                         label="Apellido Paterno"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="6" class="d-flex">
                       <v-text-field
                         v-model="apMaterno"
                         label="Apellido Materno"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="6" class="d-flex">
@@ -74,28 +87,28 @@
                       <v-text-field
                         v-model="direccion"
                         label="Dirección"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="6" class="d-flex">
                       <v-text-field
                         v-model="telefono"
                         label="Teléfono"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="6" class="d-flex">
                       <v-text-field
                         v-model="celular"
                         label="Celular"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="6" class="d-flex">
                       <v-text-field
                         v-model="correo"
                         label="Correo"
-                        disabled
+                        :disabled="disabled"
                       ></v-text-field>
                     </v-col>
                   </v-row>
@@ -119,6 +132,8 @@ export default {
     correoEmpresarial: "",
     celularEmpresarial: "",
     cargo: "",
+    guardar: false,
+    disabled: true,
   }),
   computed: {
     ...mapState(["user"]),
@@ -130,6 +145,15 @@ export default {
         local: this.idLocal,
       };
     },
+    eventoEdit() {
+      if (this.guardar === false) {
+        this.disabled = false;
+        this.guardar = true;
+      } else {
+        this.disabled = true;
+        this.guardar = false;
+      }
+    },
     datosUsuario() {
       post("usuarioDatos", this.assembleUsuarioLocal()).then((data) => {
         this.correoEmpresarial = data.CorreoEmpresarial;
@@ -139,7 +163,7 @@ export default {
         this.apPaterno = data.ApellidoPaterno;
         this.apMaterno = data.ApellidoMaterno;
         this.numeroD = data.NumeroDocumento;
-        this.direccion = data.direccion;
+        this.direccion = data.Direccion;
         this.telefono = data.Telefono;
         this.correo = data.Correo;
         this.celular = data.Celular;
